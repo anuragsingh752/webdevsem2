657721682d9590fa360f9bd8c248ac302eabac56
anurag singh <anuragthakur8661@gmail.com>
2026-05-07 11:36:04 +0530

    commit

---
+++ exp7/exp7/src/usestate.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+function Usestate() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);   // 👈 reset to zero
+  };
+
+  return (
+    <>
+      <h2>Count: {count}</h2>
+
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+    </>
+  );
+}
+
+export default Usestate;
